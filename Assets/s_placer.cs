70251d2a019eb721ce529f555814ee4642592a3a
ulimit <jrs.dmz.2014@gmail.com>
2014-10-18 17:01:46 -0400

    fixed shooting toupe

--- Assets/s_placer.cs
+++ Assets/s_placer.cs
@@ -15,21 +15,26 @@ public class s_placer : MonoBehaviour {
 	void Update () {
 		if (Input.GetMouseButtonDown(0))
 		{
-//			RaycastHit hitInfo;
+			RaycastHit hitInfo;
+			Vector3 origin = Camera.main.ScreenPointToRay(Input.mousePosition).origin;
+			Vector3 direction = Camera.main.ScreenPointToRay(Input.mousePosition).direction;
+
 //			if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo, 10000, headMask))
 //			{
-//				Vector3 direction = new Vector3(hitInfo.
+//
+//
 //				hairPoint = hitInfo.point;
 //				normal = hitInfo.normal;
 //				currentHair = Instantiate(hairPrefab) as GameObject;
 //			}
 			GameObject instantiatedToupe = Instantiate (toupe,
-			                                           transform.position,
+			                                           origin,
 			                                           transform.rotation)
 				as GameObject;
 			transform.TransformDirection(new Vector3(0,0,0));
 			foreach (Rigidbody rb in instantiatedToupe.GetComponentsInChildren<Rigidbody>()) {
-				rb.velocity = transform.TransformDirection(new Vector3(0,0,speed));
+//				rb.velocity = transform.TransformDirection(new Vector3(direction.x,direction.y,direction.z + speed));
+				rb.velocity = direction*speed;
 			}
 
 //			instantiatedToupe.rigidbody.velocity = transform.TransformDirection(new Vector3(0,0,0));
