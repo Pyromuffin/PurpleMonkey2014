8a3f31e548631d1b3cb0bdbb66c8b95c8f001726
ulimit <jrs.dmz.2014@gmail.com>
2014-10-18 15:17:50 -0400

    added sticky toupe

---
+++ Assets/s_hairTapeStick.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class s_hairTapeStick : MonoBehaviour {
+
+	public Collider thing;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter (Collision col)
+	{
+		//Debug.LogError("collided");
+		rigidbody.isKinematic = true;
+	}
+
+//	void OnTriggerEnter(Collider other) {
+		//Debug.LogError("triggered");
+//		Debug.LogWarning ("triggered");
+
+	//}
+}
