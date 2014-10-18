3cab8c181fb75e03505230d98a91608f1c516fd8
Kelly MacNeill <pyromuffin@gmail.com>
2014-10-18 19:35:52 -0400

    Coverage calculates!

---
+++ Assets/CoverageCalculator.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoverageCalculator : MonoBehaviour {
+
+    public GameObject MeshGO;
+    public LayerMask hairMask;
+
+    public float Coverage()
+    {
+        Mesh mesh = MeshGO.GetComponent<MeshCollider>().sharedMesh;
+
+        int hairsCount = 0;
+
+        
+
+        for (int i = 0; i < mesh.vertices.Length; i++)
+		{
+            var worldPos = MeshGO.transform.TransformPoint(mesh.vertices[i]);
+            var worldNormal = MeshGO.transform.TransformDirection(mesh.normals[i]);
+
+
+            Debug.DrawRay(worldPos, worldNormal, Color.white, 10);
+
+            if (Physics.CheckSphere(worldPos, .01f, hairMask)) 
+            {
+                hairsCount++;
+            }
+
+
+		}
+
+        float ratio = (float)hairsCount / (float)mesh.vertices.Length;
+
+        Debug.Log(ratio);
+        return ratio;
+    }
+
+
+	
+}
