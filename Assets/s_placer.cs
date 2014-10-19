fe2da45f0dfb1653d5006199343b910e8eb094f2
Kelly MacNeill <pyromuffin@gmail.com>
2014-10-19 15:08:31 -0400

    added horrible button of sadness

--- Assets/s_placer.cs
+++ Assets/s_placer.cs
@@ -14,7 +14,8 @@ public class s_placer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButtonDown(0) && !SceneState.instance.stylingMode)
+		/*
+        if (Input.GetMouseButtonDown(0) && !SceneState.instance.stylingMode)
 		{
 			RaycastHit hitInfo;
 			Vector3 origin = Camera.main.ScreenPointToRay(Input.mousePosition).origin;
@@ -43,5 +44,41 @@ public class s_placer : MonoBehaviour {
 //			instantiatedToupe.rigidbody.velocity = transform.TransformDirection(new Vector3(0,0,0));
 //			instantiatedToupe.velocity = transform.TransformDirection(new Vector3(0,0,speed));
 		}
+         * */
 	}
+
+    public void shootHair()
+    {
+        if (SceneState.instance.stylingMode)
+            return;
+       
+        RaycastHit hitInfo;
+        Vector3 origin = Camera.main.ScreenPointToRay(Input.mousePosition).origin;
+        Vector3 direction = Camera.main.ScreenPointToRay(Input.mousePosition).direction;
+
+        //			if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo, 10000, headMask))
+        //			{
+        //
+        //
+        //				hairPoint = hitInfo.point;
+        //				normal = hitInfo.normal;
+        //				currentHair = Instantiate(hairPrefab) as GameObject;
+        //			}
+        GameObject instantiatedToupe = Instantiate(toupe,
+                                                   origin,
+                                                   Camera.main.transform.rotation)
+            as GameObject;
+
+
+        instantiatedToupe.transform.forward = direction;
+        foreach (Rigidbody rb in instantiatedToupe.GetComponentsInChildren<Rigidbody>())
+        {
+            //				rb.velocity = transform.TransformDirection(new Vector3(direction.x,direction.y,direction.z + speed));
+            rb.velocity = (direction + Vector3.up) * speed;
+        }
+        audio.PlayOneShot(shootSound);
+        //			instantiatedToupe.rigidbody.velocity = transform.TransformDirection(new Vector3(0,0,0));
+        //			instantiatedToupe.velocity = transform.TransformDirection(new Vector3(0,0,speed));
+    }
+
 }
