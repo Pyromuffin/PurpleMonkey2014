ec7284e28f91008059da9ae66326bcac9be256c5
Kelly MacNeill <pyromuffin@gmail.com>
2014-10-18 22:16:45 -0400

    Coverage and auto baldspot now work

--- Assets/CoverageCalculator.cs
+++ Assets/CoverageCalculator.cs
@@ -1,10 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CoverageCalculator : MonoBehaviour {
 
     public GameObject MeshGO;
     public LayerMask hairMask;
+    List<int> indices = new List<int>();
+    List<int> baldSpotIndices = new List<int>();
+    bool first = true;
+
+
+    void Start()
+    {
+        Coverage();
+        first = false;
+    }
+
 
     public float Coverage()
     {
@@ -20,19 +32,28 @@ public class CoverageCalculator : MonoBehaviour {
             var worldNormal = MeshGO.transform.TransformDirection(mesh.normals[i]);
 
 
-            Debug.DrawRay(worldPos, worldNormal, Color.white, 10);
+            //Debug.DrawRay(worldPos, worldNormal, Color.white, 10);
 
-            if (Physics.CheckSphere(worldPos, .01f, hairMask)) 
+            //RaycastHit info;
+
+            if (Physics.Raycast(worldPos + worldNormal, -worldNormal, 1000, hairMask)) 
             {
+                if(first)
+                    indices.Add(i);
+
                 hairsCount++;
             }
-
+            else if (indices.Contains(i))
+            {
+                baldSpotIndices.Add(i);
+            }
 
 		}
 
         float ratio = (float)hairsCount / (float)mesh.vertices.Length;
 
-        Debug.Log(ratio);
+        Debug.Log(baldSpotIndices.Count);
+        baldSpotIndices.Clear();
         return ratio;
     }
 
